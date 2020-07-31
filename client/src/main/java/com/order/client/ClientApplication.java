98a6b46e922c2a2f0173fd56cb3da35afa54a528
zhuweiqiang <zhuweiqiang@mail.weisanyun.com>
2020-07-31 19:48:19 +0800

    Initial commit

---
+++ client/src/main/java/com/order/client/ClientApplication.java
@@ -0,0 +1,10 @@
+package com.order.client;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
---
+++ server/src/main/java/com/server/order/config/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package com.server.order.config;
+
+
+/*
+@Deprecated
+@Component
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
+*/
